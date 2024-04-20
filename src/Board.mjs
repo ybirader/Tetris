acbe62b6d92061f129303ce5984358666219816f
Yusuf Birader <yusuf.birader@hotmail.com>
2024-04-20 16:18:01 +0100

    refactor: use 2d array data structure

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,14 +1,22 @@
 export class Board {
   width;
   height;
+  grid;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.grid = []
+    for (let i = 0; i < width; i++) {
+      let row = []
+      for (let j = 0; j < height; j++) {
+        row.push(".")
+      }
+      this.grid.push(row)
+    }
   }
 
   toString() {
-    let row = ".".repeat(this.width) + "\n"
-    return row.repeat(this.height)
+    return this.grid.map(row => `${row.join("")}\n`).reduce((result, rowString) => result += rowString, "")
   }
 }
