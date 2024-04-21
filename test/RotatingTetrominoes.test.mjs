e9480b9dda40059f1346f116ee61bd935763a202
Yusuf Birader <yusuf.birader@hotmail.com>
2024-04-21 09:55:35 +0100

    add I clockwise rotation

--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -63,7 +63,7 @@ describe("The I shape", () => {
     );
   });
 
-  test.skip("can be rotated right/clockwise", () => {
+  test("can be rotated right/clockwise", () => {
     expect(shape.rotateRight().toString()).to.equalShape(
       `..I..
        ..I..
