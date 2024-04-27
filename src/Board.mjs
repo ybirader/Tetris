0d32c8d64622b8a1e1b5000f356af84b4f44f412
Yusuf Birader <yusuf.birader@hotmail.com>
2024-04-27 16:43:34 +0100

    stop tetromino when it hits bottom

--- src/Board.mjs
+++ src/Board.mjs
@@ -35,6 +35,12 @@ export class Board {
       this.grid[this.currentCoordinate.row][this.currentCoordinate.col] = this.SENTINEL_MARKER;
       return;
     }
+
+    for (let row = 0; row < this.fallingTetromino.dimension; row++) {
+      for (let col = 0; col < this.fallingTetromino.dimension; col++) {
+        this.grid[row + this.currentCoordinate.row][col + this.currentCoordinate.col] = this.SENTINEL_MARKER;
+      }
+    }
   }
 
   addTetromino() {
@@ -99,14 +105,7 @@ export class Board {
         this.grid[this.currentCoordinate.row + 1][this.currentCoordinate.col] != this.SENTINEL_MARKER
       );
     }
+
+    return this.currentCoordinate.row + this.fallingTetromino.dimension > this.height;
   }
 }
-
-/*
-
-
-- currentRow + 1 out of bounds || currentRow + tetromino width out of bounds
-
-
-
-*/
